edc9e5b523c9873a7507fa1c34b9bface0d5ac53
HARRY OMOLLOH <harryomollo95@gmail.com>
2022-10-25 10:53:01 +0300

    initial git commit

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,24 +1,82 @@
-pragma solidity ^0.6.6
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.6;
 
-contract Lottery {
+import "../interfaces/AggregatorV3Interface.sol";
+import "../interfaces/VRFConsumerBase.sol";
+import "./Ownable.sol";
+
+contract Lottery is VRFConsumerBase, Ownable {
     address payable[] public players;
     uint256 public usdEntryFee;
+    uint256 public randomness;
+    address payable public recentWinner;
     AggregatorV3Interface internal ethUsdPriceFeed;
 
-    constructor (address _priceFeedAddress) public {
-        usdEntryFee = 50 * (10**18);
-        ethUsdPriceFeed = AggregatorV3Interface();
+    /**
+    Using an Enum to create a new user defined type. They work like Structs
+    Enums are represented by numbers 0 = Open, 1 = Closed, 2 = Calculating_winner, in the below enum type
+     */
+
+    enum LOTTERY_STATE  {
+        OPEN, 
+        CLOSED,
+        CALCULATING_WINNER
     }
 
+    LOTTERY_STATE public lottery_state;
+    uint256 public fee;
+    bytes32 public keyhash;
+
+    constructor (
+        address _priceFeedAddress,
+        address _vrfCoodinator,
+        address _link,
+        uint256 _fee,
+        bytes32 _keyhash
+        ) public VRFConsumerBase(_vrfCoodinator,_link) {
+            usdEntryFee = 50 * (10**18);
+            ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+            lottery_state = LOTTERY_STATE.CLOSED;
+            fee = _fee;
+            keyhash = _keyhash;
+        }
+
     function enter() public payable{
-        players.push(msg.sender)
+        require(lottery_state == LOTTERY_STATE.OPEN,"Lottery state is not yet open");
+        require(msg.value >= getEntranceFee(),"Not enough ETH!");
+        players.push(msg.sender);
     }
 
     function getEntranceFee() public view returns(uint256) {
+        (,int price,,,) = ethUsdPriceFeed.latestRoundData();
+        uint256 adjustedPrice = uint256(price) * 10**10;
+
+        uint256 costToEnter = (usdEntryFee *10**18) / adjustedPrice;
+        return costToEnter;
 
     }
 
-    function startLottery() public {}
+    function startLottery() public onlyOwner {
+        require(lottery_state == LOTTERY_STATE.CLOSED,"Cant Start a new Lottery yet");
+        lottery_state = LOTTERY_STATE.OPEN;
+    }
+    
+    function endLottery() public onlyOwner {
+        lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
+        requestRandomness(keyhash,fee);
+    }
 
-    function endLottery() public {}
+    function fulfillRandomness(bytes32 _requestId, uint256 _randomness) internal override {
+        require(lottery_state == LOTTERY_STATE.CALCULATING_WINNER,"You are not there yet");
+        require(_randomness > 0,"Random not found");
+        uint256 indexOfWinner = _randomness % players.length;
+        recentWinner = players[indexOfWinner];
+
+        // Giving the recent winner their price
+        recentWinner.transfer(address(this).balance);
+        // Reset
+        players = new address payable[](0);
+        lottery_state = LOTTERY_STATE.CLOSED;
+        randomness = _randomness;
+    }
 }
---
+++ scripts/deploy_lottery.py
@@ -0,0 +1,7 @@
+from .helpful_scripts import get_account
+from brownie import Lottery
+def deploy_lottery():
+    account = get_account()
+
+def main():
+    deploy_lottery()
